859366eff2f95091099d3bf303a29b16d6fca40b
Jim Madge <jmmadge@gmail.com>
2020-10-29 15:47:01 +0000

    Add initial terraform and ansible

---
+++ main.tf
@@ -0,0 +1,131 @@
+# Configure the Microsoft Azure Provider.
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}RG"
+  location = var.location
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.prefix}Vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.prefix}Subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create public IP
+resource "azurerm_public_ip" "publicip" {
+  name                = "${var.prefix}PublicIP"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.prefix}NSG"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Create network interface
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.prefix}NIC"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "${var.prefix}NICConfg"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+}
+
+# Create a Linux virtual machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = "${var.prefix}VM"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  size                  = var.vm_size
+  computer_name         = "${var.prefix}VM"
+  admin_username        = var.admin_username
+
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.admin_ssh_public_key)
+  }
+
+  os_disk {
+    name                 = "${var.prefix}OsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+
+data "azurerm_public_ip" "ip" {
+  name                = azurerm_public_ip.publicip.name
+  resource_group_name = azurerm_linux_virtual_machine.vm.resource_group_name
+  depends_on          = [azurerm_linux_virtual_machine.vm]
+}
+
+output "public_ip_address" {
+  value = data.azurerm_public_ip.ip.ip_address
+}
+
+resource "local_file" "ansible_inventory" {
+  filename        = "./inventory.yaml"
+  file_permission = "0644"
+  content         = <<-DOC
+    ---
+    all:
+      hosts:
+        vm:
+          ansible_host: ${data.azurerm_public_ip.ip.ip_address}
+          ansible_user: ${var.admin_username}
+          ansible_ssh_private_key_file: ${var.admin_ssh_private_key}
+    DOC
+}
---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "vm_size" {
+  type    = string
+  default = "Standard B2s"
+}
+
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "prefix" {
+  type    = string
+  default = "my"
+}
+
+variable "admin_username" {
+  type    = string
+  default = "ansible_admin"
+}
+
+variable "admin_ssh_public_key" {
+  type    = string
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "admin_ssh_private_key" {
+  type    = string
+  default = "~/.ssh/id_rsa"
+}
