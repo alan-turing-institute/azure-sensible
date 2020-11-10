051c14dc06be2803f63f570acbb40ce0b291251a
Jim Madge <jmmadge@gmail.com>
2020-11-10 14:41:28 +0000

    Move NSG rule outside of NSG resource
    This makes the terraform configuration more useful as a template where
    additional NSG rules may be added.

--- main.tf
+++ main.tf
@@ -50,23 +50,26 @@ resource "azurerm_public_ip" "publicip" {
   allocation_method   = "Dynamic"
 }
 
-# Create Network Security Group and rule
+# Create Network Security Group
 resource "azurerm_network_security_group" "nsg" {
   name                = "${var.prefix}NSG"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
+}
 
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
+# Create SSH NSG rule
+resource "azurerm_network_security_rule" "ssh" {
+  name                           = "SSH"
+  priority                       = 1001
+  direction                      = "Inbound"
+  access                         = "Allow"
+  protocol                       = "Tcp"
+  source_port_range              = "*"
+  destination_port_range         = "22"
+  source_address_prefix          = "*"
+  destination_address_prefix     = "*"
+  resource_group_name            = azurerm_resource_group.rg.name
+  network_security_group_name    = azurerm_network_security_group.nsg.name
 }
 
 # Create network interface
