0e1d67971aa5b1c5588b672479f58ad1f3d7dfad
Jim Madge <jmmadge@gmail.com>
2020-11-10 14:52:07 +0000

    Correct terraform formatting

--- main.tf
+++ main.tf
@@ -59,17 +59,17 @@ resource "azurerm_network_security_group" "nsg" {
 
 # Create SSH NSG rule
 resource "azurerm_network_security_rule" "ssh" {
-  name                           = "SSH"
-  priority                       = 1001
-  direction                      = "Inbound"
-  access                         = "Allow"
-  protocol                       = "Tcp"
-  source_port_range              = "*"
-  destination_port_range         = "22"
-  source_address_prefix          = "*"
-  destination_address_prefix     = "*"
-  resource_group_name            = azurerm_resource_group.rg.name
-  network_security_group_name    = azurerm_network_security_group.nsg.name
+  name                        = "SSH"
+  priority                    = 1001
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
 }
 
 # Create network interface
